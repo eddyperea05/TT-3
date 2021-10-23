fc32d8ab41f772fa708418e20048736a1855c16e
eddyperea05 <eddyperea05@gmail.com>
2021-10-23 15:36:33 -0500

    Se ha terminado el punto 2 y 3

--- punto1.py
+++ punto1.py
@@ -1,18 +1,33 @@
 def has_numbers(inputString):
     return any(char.isdigit() for char in inputString)
 
-list = []
-palabra=""
-validador = True
+def mientras(list, limit,text):
+    while(len(list) < limit):
+        palabra = input(f"Por favor ingrese un {text}: ")
+        if(has_numbers(palabra)):
+            print("Contiene numero, por favor validar nuevamente")
+        else:
+            list.append(palabra)
+            print("Se ha agregado la palabra")
 
-while(validador):
-    palabra = input("Por favor ingrese un articulo de la vida cotidiana ")
-    if(has_numbers(palabra)):
-        print("Contiene numero, por favor validar nuevamente")
-    else:
-        list.append(palabra)
-        print("Se ha agregado la palabra")
-    entrada = input("Desea seguir agregado? (Si/No) ").lower()
-    if(entrada == "no"):
-        validador = False
-print(list)
+#Se agregan los artuculos de la vida cotidiana
+listArticulos = []
+text = "articulo de la vida cotidiana"
+#mientras(listArticulos,10,text)
+print(listArticulos)
+#Se agregan lo animales
+listAnimales = []
+text = "animal"
+mientras(listAnimales,7,text)
+print(listAnimales, len(listAnimales))
+opcion = input("Desea terminar con el procedimiento? (Si/No)").lower()
+if(opcion == "si"):
+    listArticulos += listAnimales
+    print(listArticulos)
+else:
+    del listArticulos[:]
+    del listAnimales[:]
+    print(listArticulos,listAnimales)
+#Transforación de lista a Tupla
+tuple = tuple(listArticulos)
+print(tuple,len(tuple))
