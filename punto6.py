74a351a242a8399bc7f797feead6e37ce87c44e9
eddyperea05 <eddyperea05@gmail.com>
2021-10-24 10:40:07 -0500

    Punto 6

---
+++ punto6.py
@@ -0,0 +1,8 @@
+mascotas = {
+    'Gato' : ["Persa","Bengala","Maine Coon", "Siamés", "Esfinge", "Ragdoll"],
+    'Perro' : ["Akita inu", "Lakeland terrier", "Labrador Retriever", "Pastor alemán", "Pit bull terrier americano", "Border collie"],
+    'Loro' : ["Cacatuoidea", "Strigopoidea", "Strigopoidea", "Amazonas", "Papagayos", "Cotorras"],
+    'Pez' : ["Bettas", "Guramis", "Cirujano", "Payaso", "Mollys ", "Platys", "Ramirezi"],
+}
+
+print(mascotas['Gato'])
