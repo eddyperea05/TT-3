2900464ea4fcb94ed71e50d6e99ec72f430811a6
eddyperea05 <eddyperea05@gmail.com>
2021-10-30 20:26:43 -0500

    Punto 6

--- punto6.py
+++ punto6.py
@@ -1,8 +1,19 @@
 mascotas = {
-    'Gato' : ["Persa","Bengala","Maine Coon", "Siamés", "Esfinge", "Ragdoll"],
-    'Perro' : ["Akita inu", "Lakeland terrier", "Labrador Retriever", "Pastor alemán", "Pit bull terrier americano", "Border collie"],
-    'Loro' : ["Cacatuoidea", "Strigopoidea", "Strigopoidea", "Amazonas", "Papagayos", "Cotorras"],
-    'Pez' : ["Bettas", "Guramis", "Cirujano", "Payaso", "Mollys ", "Platys", "Ramirezi"],
+    'Gato' : [],
+    'Perro' : [],
+    'Loro' : [],
+    'Pez' : [],
 }
 
-print(mascotas['Gato'])
+menu = "Especies. \n1. Agregar Especies\n2. Ver especies\3. Salir"
+
+while(True):
+    opcion = input("Por favor ingrese el nombre de de la especie").lower()
+    if((opcion == "Gato" or opcion == "Perro") or (opcion == "Loro" or opcion == "Pez")):
+        if(len(mascotas[opcion])<4):
+            mascotas[opcion].append(input(f"Ingrese la raza de {opcion}"))
+        else:
+            print("Esta especie ya esta llena")
+    else:
+        print("Esta especie no existe")
+    
